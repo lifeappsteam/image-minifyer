ae716ef39cc4f43bc88191bcb010135a193ecd36
Daniel Melo <danielmelogpi@gmail.com>
2018-05-03 17:22:06 -0300

    initial commit

---
+++ gulp-smartcrop-plugin.js
@@ -0,0 +1,42 @@
+const through = require('through2')
+var gm = require('gm').subClass({ imageMagick: true });
+var smartcrop = require('smartcrop-gm')
+var fs = require('fs')
+var Vinyl = require('vinyl');
+
+function applySmartCrop(file, width, height) {
+    return new Promise((resolve, reject) => {
+        smartcrop.crop(file.contents, { width, height })
+        .then(function(result) {
+          var crop = result.topCrop
+          gm(file.contents)
+            .crop(crop.width, crop.height, crop.x, crop.y)
+            .resize(width, height)
+            .toBuffer('JPG',function (err, buffer) {
+                if (err) return reject(err)
+                let newFile = new Vinyl({...file, contents: buffer})
+                console.log(newFile.relative)
+                resolve(newFile)
+            })
+        })
+    })
+    
+}
+
+
+module.exports = function({width, height}) {
+
+    var pushResult = (transformation, file, callback) => {
+        transformation.push(file)
+        callback()
+    }
+    
+    var transform = function(file, encoding, callback) {
+        let transformation = this
+        applySmartCrop(file, width, height, transformation)
+            .then((newFile) => pushResult(transformation, newFile, callback))
+            .catch((err) => callback())
+    }
+  
+    return through.obj(transform);
+}
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const imagemin = require('gulp-imagemin')
+const smartCrop = require('./gulp-smartcrop-plugin')
+
+gulp.task('default', () =>
+	gulp.src('images/*.jpg')
+        .pipe(smartCrop({
+            width: 200,
+            height: 200
+        }))
+        .pipe(imagemin([
+            imagemin.jpegtran()
+        ]))
+		.pipe(gulp.dest('dist/'))
+);
